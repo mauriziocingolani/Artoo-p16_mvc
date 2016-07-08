f1f059c41cbcdb7e45b761d8171eb933cc9a89ab
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-07-08 16:06:08 +0200

    Metodi di query su TabellaDatabase

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -10,7 +10,9 @@ class SiteController extends Controller {
     public function actionHome() {
         $utenti = array('pippo', 'pluto', 'paperino');
 //        Application::GetIstanza()->db;
-        var_dump(Utente::FindByPk(1));
+//        var_dump(Utente::FindByPk(1));
+//        var_dump(Utente::FindAll('NomeUtente DESC'));
+        var_dump(Utente::FindByCondition("NomeUtente='pippo' OR Abilitato=1"));
         # render
         $this->render('home', array('utenti' => $utenti, 'titolo' => 'Home page'));
     }
--- framework/TabellaDatabase.php
+++ framework/TabellaDatabase.php
@@ -7,7 +7,59 @@ class TabellaDatabase {
     public static function FindByPk($pk) {
         $db = Application::GetIstanza()->db;
         $query = "SELECT * FROM " . static::$nomeTabella . ' WHERE ' . static::GetPk() . '=' . $pk;
-        var_dump($query);
+        $result = $db->query($query);
+        if ($result->num_rows == 1) :
+            $obj = new static;
+            $r = $result->fetch_object();
+            foreach ($r as $prop => $value) :
+                $obj->$prop = $value;
+            endforeach;
+            return $obj;
+        endif;
+        return null;
+    }
+
+    public static function FindAll($order = null) {
+        $db = Application::GetIstanza()->db;
+        $query = "SELECT * FROM " . static::$nomeTabella;
+        if ($order) :
+            $query.=" ORDER BY " . $order;
+        endif;
+        $result = $db->query($query);
+        if ($result->num_rows > 0) :
+            $data = array();
+            while (($r = $result->fetch_object()) != null) :
+                $obj = new static;
+                foreach ($r as $prop => $value) :
+                    $obj->$prop = $value;
+                endforeach;
+                $data[] = $obj;
+            endwhile;
+            return $data;
+        endif;
+        return array();
+    }
+
+    public static function FindByCondition($condition, $order = null) {
+        $db = Application::GetIstanza()->db;
+        $query = "SELECT * FROM " . static::$nomeTabella .
+                " WHERE " . $condition;
+        if ($order) :
+            $query.=" ORDER BY " . $order;
+        endif;
+        $result = $db->query($query);
+        if ($result->num_rows > 0) :
+            $data = array();
+            while (($r = $result->fetch_object()) != null) :
+                $obj = new static;
+                foreach ($r as $prop => $value) :
+                    $obj->$prop = $value;
+                endforeach;
+                $data[] = $obj;
+            endwhile;
+            return $data;
+        endif;
+        return array();
     }
 
     protected static function GetPk() {
