b4c6f6fdc03cfeeacb74bc8e7e53016856d9f33e
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-07-08 17:01:16 +0200

    Tipologia campi su TabellaDatabase

--- framework/TabellaDatabase.php
+++ framework/TabellaDatabase.php
@@ -3,6 +3,8 @@
 class TabellaDatabase {
 
     protected static $nomeTabella;
+    protected static $pk;
+    protected static $fields;
 
     public static function FindByPk($pk) {
         $db = Application::GetIstanza()->db;
@@ -12,6 +14,9 @@ class TabellaDatabase {
             $obj = new static;
             $r = $result->fetch_object();
             foreach ($r as $prop => $value) :
+                # verificare tipologia del campo
+                # cast a intero per i valori dei campi di categoria intera
+                # assegnazione semplice per stringhe e per tutti gli altri
                 $obj->$prop = $value;
             endforeach;
             return $obj;
@@ -63,13 +68,31 @@ class TabellaDatabase {
     }
 
     protected static function GetPk() {
+        if (static::$pk == null) :
+            static::_GetFields();
+        endif;
+        return static::$pk;
+    }
+
+    protected static function GetFields() {
+        if (static::$fields == null) :
+            static::_GetFields();
+        endif;
+        return static::$fields;
+    }
+
+    protected static function _GetFields() {
+        if (static::$pk || is_array(static::$fields))
+            return;
         $db = Application::GetIstanza()->db;
         $query = "SHOW COLUMNS FROM " . static::$nomeTabella;
         $result = $db->query($query);
+        static::$fields = array();
         while (($r = $result->fetch_object()) != null) :
             if ($r->Key == 'PRI') :
-                return $r->Field;
+                static::$pk = $r->Field;
             endif;
+            static::$fields[$r->Field] = $r->Type;
         endwhile;
     }
 
